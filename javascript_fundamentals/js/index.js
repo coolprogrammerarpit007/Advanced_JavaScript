1117daad7909ecffc0d824e3dd244b9bd6bc2176
arpit mishra <arpit.mishra.out@gmail.com>
2024-08-15 10:39:48 +0530

    first commit

---
+++ javascript_fundamentals/js/index.js
@@ -0,0 +1,9 @@
+`use strict`;
+// console.log(`hello world`);
+
+// JavaScript Engine
+// Javascript engine written in c++
+
+// Interpreters and Compilers
+// Interpretor:- takes the codes, parses the code and executes line by line.
+// Compiler :- Takes the whole code, converts into machine code then this machine code is sent to cpu which executes that machine code to run the program.
