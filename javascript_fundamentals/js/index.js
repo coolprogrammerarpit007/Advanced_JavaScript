1edefa67cc23117c5a6b87ce80dd39a3353e4ab9
arpit mishra <arpit.mishra.out@gmail.com>
2024-08-15 11:06:23 +0530

    JavaScript Fundamentals

--- javascript_fundamentals/js/index.js
+++ javascript_fundamentals/js/index.js
@@ -7,3 +7,13 @@
 // Interpreters and Compilers
 // Interpretor:- takes the codes, parses the code and executes line by line.
 // Compiler :- Takes the whole code, converts into machine code then this machine code is sent to cpu which executes that machine code to run the program.
+
+// Babel:- babel is a javascript compiler that takes your modern javascript code and returns browser compatible older JS code.
+// Typescript is a superset of javascript that compiles down to javascript.
+
+// Inside the V8 Engine
+
+// 1. Interpretor takes little time to up and running where as compiler takes all the time while doing calculation then upto runnning.
+// 2. Compiler does code optimization which produces faster code result after startoff where as Interpretor results slow code because it not provide any code optimizations.
+
+// so to optimize and using for advantages of both the Interpretor and the compiler, browsers started using JIT(JUST IN TIME) Compilers
